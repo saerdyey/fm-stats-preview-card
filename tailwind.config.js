e5b15f7a7fcb5c149ac8bbf96cfbbea5b68adfad
Jay Anton Roblico <jayantonroblico23@gmail.com>
2021-10-12 11:05:27 +0800

    Integrate TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      primary: "hsl(233, 47%, 7%)",
+      card: "hsl(244, 38%, 16%)",
+      accent: "hsl(277, 64%, 61%)",
+      heading: "hsl(0, 0%, 100%)",
+      paragraph: "hsla(0, 0%, 100%, 0.75)",
+      stats: "hsla(0, 0%, 100%, 0.6)",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
